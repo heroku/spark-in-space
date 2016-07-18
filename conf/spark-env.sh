22044d292935335784240b7fe8996a6b24d1f25a
Scott Clasen <scott@heroku.com>
2016-07-17 17:11:37 -0700

    initial support for HA spark masters

--- conf/spark-env.sh
+++ conf/spark-env.sh
@@ -4,3 +4,9 @@
 export SPARK_LOCAL_IP=$HEROKU_PRIVATE_IP
 
 export SPARK_PUBLIC_DNS=$HEROKU_DNS_DYNO_NAME
+
+# if there is a KAFKA_ZOOKEEPER_URL set, setup HA for spark masters
+if ! [ -z "$KAFKA_ZOOKEEPER_URL" ]; then
+  zkurl=$(echo $KAFKA_ZOOKEEPER_URL | sed 's/zookeeper\:\/\///g')
+  export SPARK_DAEMON_JAVA_OPTS="-Dspark.deploy.recoveryMode=ZOOKEEPER -Dspark.deploy.zookeeper.dir=/spark -Dspark.deploy.zookeeper.url=$zkurl"
+fi
